1eea73497c64a2b02f32d10a48c9c78475913e65
mohammedanshab <mohammed.anshab@ultsglobal.com>
2024-07-30 17:31:01 +0530

    Files structure Completed

---
+++ glamsilks/src/components/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProductList() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='border p-4'>
+            <h2 className='text-lg font-bold'>Glam Silks</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste rerum eaque natus! Autem omnis ea pariatur recusandae. Aut tenetur in officiis temporibus ipsa eveniet culpa adipisci? Quas sequi reprehenderit illo?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam aliquam veniam consequatur pariatur, ducimus debitis vel sunt eos in veritatis quisquam rem quam autem iusto omnis, ipsam neque. Totam, blanditiis.</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductList
